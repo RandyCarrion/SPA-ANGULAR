58b1537fb98b1da096397783396701d30c09ee2b
Randy Carrion <randycarrion@randys-mbp.ams.nl.ibm.com>
2018-08-24 13:19:17 +0200

    start of SPA Angular

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  posts = [
+    {title: 'First Post', content: 'this is the first post\'s content'},];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
